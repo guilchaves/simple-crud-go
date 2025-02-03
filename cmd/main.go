25466ec289ccc6ea21b05f1dab30e271fe655c62
guilherme chaves <chaves.guilherme@proton.me>
2025-02-03 16:55:00 -0300

    feat: add create product

--- cmd/main.go
+++ cmd/main.go
@@ -27,6 +27,7 @@ func main() {
 	})
 
 	server.GET("/products", ProductController.GetProducts)
+	server.POST("/product", ProductController.CreateProduct)
 
 	server.Run(":8080")
 }
--- repository/product_repository.go
+++ repository/product_repository.go
@@ -18,32 +18,52 @@ func NewProductRepository(connection *sql.DB) ProductRepository {
 
 func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
 
-    query := "SELECT id, product_name, price FROM product"
-    rows, err := pr.connection.Query(query)
-    if(err != nil){ 
-        fmt.Println(err)
-        return []model.Product{}, err
-    }
+	query := "SELECT id, product_name, price FROM product"
+	rows, err := pr.connection.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return []model.Product{}, err
+	}
 
-    var productList []model.Product
-    var productObj model.Product
+	var productList []model.Product
+	var productObj model.Product
 
-    for rows.Next(){
-        err = rows.Scan(
-            &productObj.ID,
-            &productObj.Name,
-            &productObj.Price,
-            )
-        if err != nil {
-            fmt.Println(err)
-            return []model.Product{}, err
-        }
-        productList = append(productList, productObj)
-    }
+	for rows.Next() {
+		err = rows.Scan(
+			&productObj.ID,
+			&productObj.Name,
+			&productObj.Price,
+		)
+		if err != nil {
+			fmt.Println(err)
+			return []model.Product{}, err
+		}
+		productList = append(productList, productObj)
+	}
 
-    rows.Close()
+	rows.Close()
 
-    return productList, nil
+	return productList, nil
+}
 
+func (pr *ProductRepository) CreateProduct(product model.Product) (int, error) {
 
+	var id int
+	query, err := pr.connection.Prepare("INSERT INTO product" +
+		" (product_name, price)" +
+		" VALUES ($1, $2) RETURNING id")
+
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	err = query.QueryRow(product.Name, product.Price).Scan(&id)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+    query.Close()
+    return id, nil
 }
