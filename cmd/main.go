6c4e738e7a254d9a05ff001c3d0c15a7acc5014e
guilherme chaves <chaves.guilherme@proton.me>
2025-02-03 16:24:25 -0300

    chore: add first get route

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"go-gin-api/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	server := gin.Default()
+
+	ProductController := controller.NewProductController()
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+    server.GET("/products", ProductController.GetProducts)
+
+	server.Run(":8080")
+}
